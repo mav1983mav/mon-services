83aeac3b591fd7e4c1bdee250f0da7b26129ce45
Alexandr Marasanov <mav1983@mail.ru>
2024-12-06 13:45:50 +0700

    clean the svcript for task and unit systemd file

--- mon-services.sh
+++ mon-services.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
-
 while true; do
 IFS=$'\n'
-service="nginx.service" ###
-monlog="/www/monitoring.log" ###
+service="test.service"
+monlog="/var/log/monitoring.log"
 status=$(systemctl is-failed $service)
 datecur=$(date '+%Y-%m-%dT%H:%M:%S%z')
 testping=$(ping -c 1 8.8.8.8 | grep loss | awk '{print $6}' | cut -b -1)
     if [ "$status" == "inactive" ]; then
-    echo "1" ###
+    echo ""
     else
 	if [ "$status" == "active" ]; then
-	    echo "0" ###
-#	    curl "https://test.com/monitoring/test/api"
+	    curl "https://test.com/monitoring/test/api"
 	fi
 	for j in $(journalctl -o short-iso -u $service --since "1 min ago" | grep -e 'Started' -e 'Reloaded' | awk '{print $1,$4}' | awk '{sub("Started", "Started / Restarted", $0)}1'); do
 	    resdat=$(echo "$j" | awk '{print $1}')
@@ -26,6 +23,5 @@ testping=$(ping -c 1 8.8.8.8 | grep loss | awk '{print $6}' | cut -b -1)
     else
 	echo "$datecur host $HOSTNAME not available" >> $monlog
     fi
-
     sleep 60
 done
