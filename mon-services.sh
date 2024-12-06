44cf329470ffeb7d757c718978f96f625978de3a
Alexandr Marasanov <mav1983@mail.ru>
2024-12-06 11:24:55 +0700

    add monitoring log Restart and Reload to file

--- mon-services.sh
+++ mon-services.sh
@@ -2,18 +2,22 @@
 
 
 while true; do
-service="nginx.service"
-monlog="/www/monitoring.log"
+IFS=$'\n'
+service="nginx.service" ###
+monlog="/www/monitoring.log" ###
 status=$(systemctl is-failed $service)
     if [ "$status" == "inactive" ]; then
-    echo "1"
+    echo "1" ###
     else
 	if [ "$status" == "active" ]; then
-	    echo "0"
+	    echo "0" ###
 #	    curl "https://test.com/monitoring/test/api"
 	fi
-	
-	IFS=$'\n'
+	for j in $(journalctl -o short-iso -u $service --since "100 min ago" | grep -e 'Started' -e 'Reloaded' | awk '{print $1,$4}' | awk '{sub("Started", "Started / Restarted", $0)}1'); do
+	    resdat=$(echo "$j" | awk '{print $1}')
+	    resstat=$(echo "$j" | awk '{print substr($0,26)}')
+	    echo "$resdat $service was $resstat" >> $monlog
+	done
     fi
 
 
